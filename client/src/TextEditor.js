c10ca0e00e3e1c804a67101257af4de74ae1de2c
Djurre <djurre10@hotmail.com>
2021-05-03 23:58:49 +0200

    Setup server to client connection

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from 'quill'
+import {io} from 'socket.io-client'
 
 import 'quill/dist/quill.snow.css'
 
@@ -16,6 +17,18 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
+
+    useEffect(() => {
+        const s = io("http://localhost:3001")
+        setSocket(s)
+
+        return () => {
+            s.disconnect()
+        }
+    }, [])
+
     // When the html with ref wrapperRef is created execute this code
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return
@@ -25,7 +38,8 @@ export default function TextEditor() {
         const editor = document.createElement('div')
         wrapper.append(editor)
 
-        new Quill(editor, {theme: 'snow', modules: {toolbar:TOOLBAR_OPTIONS}})
+        const q = new Quill(editor, {theme: 'snow', modules: {toolbar:TOOLBAR_OPTIONS}})
+        setQuill(q)
     }, [])
 
     return <div class="container" ref={wrapperRef}></div>
