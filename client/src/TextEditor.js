0f667027a239761e90abdd8f888c2e2d6951abc0
Djurre <djurre10@hotmail.com>
2021-05-04 14:50:57 +0200

    Added MongoDB to store documents

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,9 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import Quill from 'quill'
 import {io} from 'socket.io-client'
+import {useParams} from 'react-router-dom'
 
 import 'quill/dist/quill.snow.css'
 
+const SAVE_INTERVAL_MS = 2000
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3]}],
     [{ font: []}],
@@ -16,6 +18,7 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const {id: documentId} = useParams()
     const [socket, setSocket] = useState()
     const [quill, setQuill] = useState()
 
@@ -28,6 +31,31 @@ export default function TextEditor() {
             s.disconnect()
         }
     }, [])
+
+    // Get a document by its id
+    useEffect(() => {
+        if(socket == null || quill == null) return
+
+        socket.once('load-document', document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+
+        socket.emit('get-document', documentId)
+    }, [socket, quill, documentId])
+
+    // Automatically save the document
+    useEffect(() => {
+        if(socket == null || quill == null) return
+
+        const interval = setInterval(() => {
+            socket.emit('save-document', quill.getContents())
+        }, SAVE_INTERVAL_MS)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [socket, quill])
 
     // Recieve changes from the server and add them to quill
     useEffect(() => {
@@ -68,6 +96,8 @@ export default function TextEditor() {
         wrapper.append(editor)
 
         const q = new Quill(editor, {theme: 'snow', modules: {toolbar:TOOLBAR_OPTIONS}})
+        q.disable()
+        q.setText('Loading...')
         setQuill(q)
     }, [])
 
